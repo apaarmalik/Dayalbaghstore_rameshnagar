f4df94cf6bf3cbac2c6181a7cf16f1b5c56e4277
code_enthu <144600507+apaarmalik@users.noreply.github.com>
2024-11-23 02:06:33 +0530

    app.py

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 import os
+import glob
 
 # Title
 st.image("G:\pullover1.png")
 
+img_files=glob.glob("G:\Malik clothing & accessories/*.png")
+st.write(len(img_files))
 
 
 
