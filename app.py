a717f75fb7fc95eadf49bb480bdbae0ae96dfca5
code_enthu <144600507+apaarmalik@users.noreply.github.com>
2024-11-23 00:53:41 +0530

    app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 st.write("hello")
 
-img = Image.open('sample.png')
+img = Image.open('brand.png')
 st.image(img, caption='Sunrise by the mountains')
 
 fle = st.file_uploader("Upload the file",accept_multiple_files=True)
