097c1111b1f1b7d0e3f6197939ca1cb8f984abe9
code_enthu <144600507+apaarmalik@users.noreply.github.com>
2024-11-23 00:20:58 +0530

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 import openpyxl as px
-import os
-import io
 
 
-file = st.file_uploader("Select an excel File", accept_multiple_files=False)
+
+st.write("hello")
