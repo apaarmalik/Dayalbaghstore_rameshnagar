07863d5f2f1f7f402caefcac689d2ce67a503967
code_enthu <144600507+apaarmalik@users.noreply.github.com>
2024-11-23 02:07:19 +0530

    app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import glob
 # Title
 st.image("G:\pullover1.png")
 
-img_files=glob.glob("G:\Malik clothing & accessories/*.png")
+img_files=glob.glob("G:\Malik clothing & accessories\*.png")
 st.write(len(img_files))
 
 
