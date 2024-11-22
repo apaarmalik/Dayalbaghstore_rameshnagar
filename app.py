a86a8bb4456d385094e0ed22dab98cbb294ed2ff
code_enthu <144600507+apaarmalik@users.noreply.github.com>
2024-11-23 01:49:30 +0530

    app.py

--- app.py
+++ app.py
@@ -4,11 +4,8 @@ from PIL import Image
 import os
 
 # Title
-st.image("G:/pullover1.png", caption="Pullover Image")
+st.image("sample.png", caption="Pullover Image")
 
-
-# Set the desired working directory
-st.image(r'G/pullover1.png', caption="Sample Image")
 
 
 
