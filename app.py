76b24db6ceab6e872374da107c7b81216c0e5816
code_enthu <144600507+apaarmalik@users.noreply.github.com>
2024-11-23 00:27:42 +0530

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 import pandas as pd
+from PIL import Image
 
 st.write("hello")
+
+img=st.file_uploader("Upload the file",accept_multiple_files=False)
+
+st.image(img, caption='Sunrise by the mountains')
