dfb97a90a526289adfc618fa6f66a636f3d9a6df
code_enthu <144600507+apaarmalik@users.noreply.github.com>
2024-12-08 20:55:13 +0530

    app.py

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ import pandas as pd
 from PIL import Image
 import os
 import glob
+from streamlit_sortables import sort_items
 
 # Title
 
 img_files=glob.glob("newsubfolder./*.png")
+sorted_images=sort_items(img_files)
 for imgs in img_files:
   st.image(imgs, caption = imgs)
 
